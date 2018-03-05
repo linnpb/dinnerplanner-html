1a2558627996e568a759adab9109d3f6fbe7ba50
linnpb <linnpb@kth.se>
2018-03-05 12:20:10 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,27 +2,79 @@ $(function() {
 
 	var model = new DinnerModel();
 
-	$("#indexView").show();
-	$("#header").show();
-	$("#sidebarView").hide();
-	$("#dishSearchView").hide();
-	$("#dishOverView").hide();
-	$("#dinnerOverview").hide();
-
 	var indexView = new IndexView($("#indexView"), model);
+	var indexController = new IndexController(indexView, model, this);
+
 	var sidebarView = new SidebarView($("#sidebarView"), model);
+	var sidebarController = new SidebarViewController(sidebarView, model, this);
+
 	var dishView = new DishView($("#dishSearchView"), model);
+	var dishviewController = new DishController(dishView, model, this);
+
 	var detailView = new DetailView($("#dishOverView"), model);
+	var detailController = new DetailController(detailView, model, this);
+
 	var overView = new OverView($("#dinnerOverview"), model);
+	var overviewController = new OverViewController(overView, model, this);
 
-	var indexController = new IndexController(indexView, model);
-	var sidebarController = new SidebarViewController(sidebarView, model);
-	var dishviewController = new DishController(dishView, model);
-	var detailController = new DetailController(detailView, model);
-	var overviewController = new OverViewController(overView, model);
+	var printoutView = new PrintView($("#printoutView"), model);
+	var printoutController = new PrintController(printoutView, model, this);
+
+	$(document).ajaxSend(function(event, request, settings) {
+  		$('#loading').show();
+	});
+
+	$(document).ajaxComplete(function(event, request, settings) {
+ 		$('#loading').hide();
+	});
 
 
+	this.printPage = function() {
+		indexView.hide();
+		dishView.hide();
+		overView.hide();
+		sidebarView.hide();
+		detailView.hide();
+		printoutView.show();
+	}
 
+	this.detailPage = function() {
+		indexView.hide();
+		dishView.hide();
+		overView.hide();
+		printoutView.hide();
+		sidebarView.show();
+		detailView.show();
+	}
+
+	this.overViewPage = function() {
+		indexView.hide();
+		sidebarView.hide();
+		dishView.hide();
+		detailView.hide();
+		printoutView.hide();
+		overView.show();
+	}
+
+	this.dishPage = function() {
+		indexView.hide();
+		detailView.hide();
+		overView.hide();
+		printoutView.hide();
+		sidebarView.show();
+		dishView.show();
+	}
+
+	this.homePage = function() {
+		sidebarView.hide();
+		dishView.hide();
+		detailView.hide();
+		overView.hide();
+		printoutView.hide();
+		indexView.show();
+	}
+
+	this.homePage();
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
